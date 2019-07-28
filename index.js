301014026a0d8567fecea8cbca60a585fbbf76b4
Levi Smith <Levi.charles.smith@gmail.com>
2019-07-28 16:56:48 -0700

    Configuring logger settings for debuging

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ var logger = require('winston');
 const sqlite3 = require("sqlite3");
 var sql = new sqlite3.Database("./spells");
 var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+	colorize: true
+});
+logger.level = 'debug';
