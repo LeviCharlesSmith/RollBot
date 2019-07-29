ced59afb47821df4400ae743df3a9e29d4526f4e
Levi Smith <Levi.charles.smith@gmail.com>
2019-07-28 17:56:33 -0700

    Beautify Indentation

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ logger.level = 'debug';
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+  token: auth.token,
+  autorun: true
 });
 
 function spell(name, callback) {
-  var spellData = sql.get("SELECT * FROM spells WHERE LOWER(name) = '"+ name.toLowerCase() + "'", [], (err,row) => {
-  if (err) {
-  return console.error(err.message);
-  }
+  var spellData = sql.get("SELECT * FROM spells WHERE LOWER(name) = '" + name.toLowerCase() + "'", [], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
 
-  return row
-  ? callback(row)
-  : console.log('No row found.');
+    return row ?
+      callback(row) :
+      console.log('No row found.');
   });
   return spellData;
 }
@@ -34,14 +34,14 @@ function spell(name, callback) {
 //Returns help text
 function help() {
   return "Hi! I'm RollBot and here are all my commands: \n" +
-   "`" + "/help" + "`" + "   your reading this now! \n" +
-   "`" + "/roll 1d20"  +  "`" + "   roll a die, use + and - to add modifiers \n" +
-   "`" + "/roll stats" +   "`" + "   generate your attributes by typing /roll stats \n" +
-   "`" + "/order set [name1] [name2]" +  "`" + "   save your turn order so you don’t forget \n" +
-   "`" + "/order list" + "`" + "   view the current turn order \n" +
-   "`" + "/order add [name]" + "`" + "   add someone to the end of the turn order \n" +
-   "`" + "/view" + "`" + "   view the current turn order \n" +
-   "`" + "/spell [spellname]" + "`" + "view info on any spell in the database";
+    "`" + "/help" + "`" + "   your reading this now! \n" +
+    "`" + "/roll 1d20" + "`" + "   roll a die, use + and - to add modifiers \n" +
+    "`" + "/roll stats" + "`" + "   generate your attributes by typing /roll stats \n" +
+    "`" + "/order set [name1] [name2]" + "`" + "   save your turn order so you don’t forget \n" +
+    "`" + "/order list" + "`" + "   view the current turn order \n" +
+    "`" + "/order add [name]" + "`" + "   add someone to the end of the turn order \n" +
+    "`" + "/view" + "`" + "   view the current turn order \n" +
+    "`" + "/spell [spellname]" + "`" + "view info on any spell in the database";
 }
 
 //Stores turn order.
@@ -65,91 +65,90 @@ function orderDisplay() {
 
 // Does the math for generating dice rolls
 function generateRoll(times = 1, arg1 = 2, arg2 = 0, arithmatic, bonus = 0, comment = "") {
-    var rolls = [];
+  var rolls = [];
   var sides = arg1 + "" + arg2;
-        console.log(times, sides);
-    for (var i = 0; times > i; i++) {
-        rolls.push(Math.floor(Math.random() * sides) + 1);
-    }
-    console.log(rolls);
-    return rolls;
+  console.log(times, sides);
+  for (var i = 0; times > i; i++) {
+    rolls.push(Math.floor(Math.random() * sides) + 1);
+  }
+  console.log(rolls);
+  return rolls;
 }
 
-bot.on('ready', function (evt) {
+bot.on('ready', function(evt) {
   logger.info('Connected');
   logger.info('Logged in as: ');
   logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', function(user, userID, channelID, message, evt) {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
   if (message.substring(0, 1) == '/') {
-  var args = message.substring(1).split(' ');
-  var cmd = args[0];
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
 
-  args = args.splice(1);
-  switch(cmd) {
+    args = args.splice(1);
+    switch (cmd) {
 
-  //  !spell
-  case 'spell':
-  spell(args[0], c => {
-  bot.sendMessage({
-  to: channelID,
-  message:
-  "<@!" + userID + ">\n" +
-  "```\n" +
-  "Spell Name: "   + c.name         + "\n" +
-  "Casting Time: " + c.casting_time + "\n" +
-  "Description: "  + c.description  + "\n" +
-  "Duration: "     + c.duration     + "\n" +
-  "Spell Level: "  + c.level        + "\n" +
-  "Range: "        + c.range        + "\n" +
-  "School: "       + c.school       + "\n" +
-  "```"
-  });
-  });
-  break;
-
-      //	!help
-      case 'help':
-        bot.sendMessage({
-      	   to: channelID,
-      	   message: "<@!" + userID + ">\n\n" + help()
-        });
-      break;
-
-  //!roll
-        case 'roll':
-          args = args.join().split("");
-          var rolls = generateRoll(args[0], args[2], args[3], args[4], args[5], args[6], args [7]);
-
-  if (args[4] === "+") {
+      //  !spell
+      case 'spell':
+        spell(args[0], c => {
           bot.sendMessage({
             to: channelID,
-  message:  "<@!" + userID + ">\n" +
- 			            "`" + args[0] + "d" + args[2] + args[3] + "+" + args[5] + "`" + ": " + (parseInt(rolls) + parseInt(args[5] + args[6] + args[7]))
-  });
-  } else if (args[4] === '-') {
-  bot.sendMessage({
+            message: "<@!" + userID + ">\n" +
+              "```\n" +
+              "Spell Name: " + c.name + "\n" +
+              "Casting Time: " + c.casting_time + "\n" +
+              "Description: " + c.description + "\n" +
+              "Duration: " + c.duration + "\n" +
+              "Spell Level: " + c.level + "\n" +
+              "Range: " + c.range + "\n" +
+              "School: " + c.school + "\n" +
+              "```"
+          });
+        });
+        break;
+
+        //	!help
+      case 'help':
+        bot.sendMessage({
+          to: channelID,
+          message: "<@!" + userID + ">\n\n" + help()
+        });
+        break;
+
+        //!roll
+      case 'roll':
+        args = args.join().split("");
+        var rolls = generateRoll(args[0], args[2], args[3], args[4], args[5], args[6], args[7]);
+
+        if (args[4] === "+") {
+          bot.sendMessage({
             to: channelID,
-  message:  "<@!" + userID + ">\n" +
-   			            "`" + args[0] + "d" + args[2] + args[3] + "-" + args[5] + "`" + ": " + (parseInt(rolls) - parseInt(args[5] + args[6] + args[7]))
-  });
-  } else {
-  bot.sendMessage({
+            message: "<@!" + userID + ">\n" +
+              "`" + args[0] + "d" + args[2] + args[3] + "+" + args[5] + "`" + ": " + (parseInt(rolls) + parseInt(args[5] + args[6] + args[7]))
+          });
+        } else if (args[4] === '-') {
+          bot.sendMessage({
             to: channelID,
-  message:  "<@!" + userID + ">\n" +
-   			            "`" + args[0] + "d" + args[2] + args[3] + "`" + ": " + (parseInt(rolls))
-  });
-  }
-              break;
-      //	!order
+            message: "<@!" + userID + ">\n" +
+              "`" + args[0] + "d" + args[2] + args[3] + "-" + args[5] + "`" + ": " + (parseInt(rolls) - parseInt(args[5] + args[6] + args[7]))
+          });
+        } else {
+          bot.sendMessage({
+            to: channelID,
+            message: "<@!" + userID + ">\n" +
+              "`" + args[0] + "d" + args[2] + args[3] + "`" + ": " + (parseInt(rolls))
+          });
+        }
+        break;
+        //	!order
       case 'order':
         var subcmd = args[0];
 
         args = args.splice(1);
-        switch(subcmd) {
+        switch (subcmd) {
           case 'list':
             var returnString = "";
             var returnIndex = 0;
@@ -158,20 +157,19 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             }
             bot.sendMessage({
               to: channelID,
-              message:
-                "<@!" + userID + ">\n" +
+              message: "<@!" + userID + ">\n" +
                 "The turn order is:\n" +
                 returnString
             });
-          break;
+            break;
           case 'add':
             orderAdd(args[0]);
-          break;
+            break;
           case 'set':
             orderSet(args);
-          break;
+            break;
         }
-      break;
-   }
-   }
+        break;
+    }
+  }
 });
